46ea53c7513c9ab9cc65c5785500b72675f73d29
Chris Kruining <c.kruining@fyn.nl>
2022-10-27 00:25:09 +0200

    Delete index.node.ts

--- src/index.node.ts
+++
@@ -1,6 +0,0 @@
-import { clientWithFetcher } from './client.js';
-import { fetch } from './fetch.js';
-
-export const BaseClient = clientWithFetcher(fetch);
-export { methodDescriptor } from './client.js';
-export { fetch } from './fetch.js';
